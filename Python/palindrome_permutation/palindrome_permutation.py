25e682752b5f53cb245bb8ca5040b618490495d0
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2015-11-27 22:46:18 -0800

    expressive lambda

---
+++ Python/palindrome_permutation/palindrome_permutation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def canPermutePalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # odd, one odd
+        # even, all even
+        counts = {}
+        for char in s:
+            if char not in counts:
+                counts[char] = 1
+            else:
+                counts[char] += 1
+        
+        odd_count = reduce(lambda init, v: init + 1 if v % 2 else init, counts.values(), 0)
+        
+        if len(s) % 2 == 0:
+            return odd_count == 0
+        else:
+            return odd_count == 1
