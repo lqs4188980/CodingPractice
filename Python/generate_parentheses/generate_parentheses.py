227dab51f2f59333ba1e8bbc25284c7905f5eabc
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2015-11-28 22:18:52 -0800

    backtracking

---
+++ Python/generate_parentheses/generate_parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        left = n
+        right = n
+        result = []
+        curr = ''
+        self.generate(curr, left, right, result)
+        return result
+        
+    def generate(self, curr, left, right, result):
+        if left > right:
+            return
+        if left == 0 and right == 0:
+            result.append(curr)
+        if left:
+            self.generate(curr + '(', left - 1, right, result)
+        self.generate(curr + ')', left, right - 1, result)
