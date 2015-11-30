1d7262bb90ff3c41290fbd9c760790b3c7b5bd71
Xiaoqin Zhu <xiaoqin.zhu.4@gmail.com>
2015-11-29 19:37:23 -0800

    todo

--- Python/word_break_ii/word_break_ii.py
+++ Python/word_break_ii/word_break_ii.py
@@ -1,16 +1,16 @@
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: Set[str]
-        :rtype: List[str]
-        """
-        solved = []
-        self.next_word(s, '', wordDict, solved)
-        return results
+# class Solution(object):
+#     def wordBreak(self, s, wordDict):
+#         """
+#         :type s: str
+#         :type wordDict: Set[str]
+#         :rtype: List[str]
+#         """
+#         solved = []
+#         self.next_word(s, '', wordDict, solved)
+#         return results
         
-    def next_word(self, s, curr, wordDict, solved):
-    	if s == '':
-    		return solved[curr] 
-    	if s in solved:
-    		return solved[s]
+#     def next_word(self, s, curr, wordDict, solved):
+#     	if s == '':
+#     		return solved[curr] 
+#     	if s in solved:
+#     		return solved[s]
