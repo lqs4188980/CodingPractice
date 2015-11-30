e69b9fff9806269ef909f1a6eac99bf8c7b12ba6
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2015-11-29 16:47:03 -0800

    Q: possible to use head | tail?

--- Shell/tenth_line/tenth_line.sh
+++ Shell/tenth_line/tenth_line.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-COUNTER=0
-cat file.txt | \
-while read -r line || [[ -n "$line" ]]
-do
-	let COUNTER++
-	if [ $COUNTER = 10 ]
-		then
-		echo "text read from file: $line"
-	fi
-done
+
+# extremely slow
+#COUNTER=0
+#cat file.txt | \
+#while read line
+#do
+#	COUNTER=$((COUNTER+1))
+#	if [ $COUNTER = 10 ]
+#		then
+#		echo "$line"
+#	fi
+#done
+
+sed -n "10p" file.txt
+#sed '10q;d' file.txt
+
+#how to?
+#head -n 10 file.txt | tail -n 1
