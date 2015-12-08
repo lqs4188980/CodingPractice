ba77d7787a914fbb5fc96a1a0a3b52a74233c0a9
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2015-12-07 19:45:05 -0800

    tricky, Python passing by assignment

---
+++ Python/maximum_depth_of_binary_tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,14 @@
+class Solution(object):
+	def maxDepth(self, root):
+		max_depth = [0]
+		self.helper(root, 0, max_depth)
+		return max_depth
+
+	def helper(self, root, curr, max_depth):
+		if root is None:
+			max_depth = max_depth \
+				if max_depth[0] > curr else [curr]
+			return
+		curr += 1
+		self.helper(root.left, curr, max_depth)
+		self.helper(root.right, curr, max_depth)
---
+++ Python/path_sum_ii/path_sum_ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+	def pathSum(self, root, sum):
+		results = []
+		self.helper(root, [], results, sum)
+		return results
+
+	def helper(self, root, curr, results, s):
+		if root is None:
+			return
+		s -= root.val
+		new_curr = curr + [root.val]
+		if s == 0 and root.left is None and root.right is None:
+			results.append(new_curr)
+			return
+
+		self.helper(root.left, new_curr, results, s)
+		self.helper(root.right, new_curr, results, s)
