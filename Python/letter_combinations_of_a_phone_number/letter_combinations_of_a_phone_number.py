fe509e2ce077d624476471e01770bcff9f583443
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2015-12-11 18:08:10 -0800

    speed

---
+++ Python/letter_combinations_of_a_phone_number/letter_combinations_of_a_phone_number.py
@@ -0,0 +1,29 @@
+class Solution(object):
+	def letterCombinations(self, digits):
+		mapping = {}
+		mapping['2'] = ['a', 'b', 'c']
+		mapping['3'] = ['d', 'e', 'f']
+		mapping['4'] = ['g', 'h', 'i']
+		mapping['5'] = ['j', 'k', 'l']
+		mapping['6'] = ['m', 'n', 'o']
+		mapping['7'] = ['p', 'q', 'r', 's']
+		mapping['8'] = ['t', 'u', 'v']
+		mapping['9'] = ['w', 'x', 'y', 'z']
+		results = []
+		if digits == '':
+			return results
+		self.helper(digits, 0, '', results, mapping)
+		return results
+
+	def helper(self, digits, idx, curr, results, mapping):
+		if len(curr) == len(digits):
+			results.append(curr)
+			return
+		for char in mapping[digits[idx]]:
+			new_curr = curr + char
+			self.helper(digits, idx+1, new_curr, results, mapping)
+
+if __name__ == '__main__':
+	s = '23'
+	so = Solution()
+	print(so.letterCombinations(s))
---
+++ Python/linked_list_cycle_ii/linked_list_cycle_ii.py
@@ -0,0 +1,24 @@
+class Solution(object):
+	def detectCycle(self, head):
+		p = head
+		q = head
+		while p is not None and q is not None:
+			p = p.next
+			q = q.next
+			if p is None or q is None:
+				return None
+			q = q.next
+			if q is None:
+				return None
+			if p == q:
+				break
+
+		if p is None or q is None:
+			return None
+
+		p = head
+		while p != q:
+			p = p.next
+			q = q.next
+
+		return p
