499278b3de00c88e0a2fdf0db7fe841605028758
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2015-12-17 23:13:16 -0800

    add_digits, num==0

---
+++ Python/delete_node_in_a_linked_list/delete_node_in_a_linked_list.py
@@ -0,0 +1,17 @@
+# 1st
+class Solution(object):
+	def deleteNode(self, node):
+		curr = node
+		while curr.next:
+			curr.val = curr.next.val
+			if not curr.next.next:
+				curr.next = None
+			else:
+				curr = curr.next
+
+# 2ed
+class Solution(object):
+	def deleteNode(self, node):
+		node.val = node.next.val
+		node.next = node.next.next
+
