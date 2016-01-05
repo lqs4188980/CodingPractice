ad04f5022000e11786b66426a571f93528f7a0b6
psykokwak4 <xiaoqin.zhu.4@gmail.com>
2016-01-04 19:22:11 -0800

    faster

---
+++ Python/excel_sheet_column_title/excel_sheet_column_title.py
@@ -0,0 +1,10 @@
+class Solution(object):
+	def convertToTitle(self, n):
+		res = ""
+		while n:
+			d = n % 26 - 1
+			d = 25 if d < 0 else d
+			res = res + chr(d + ord('A'))
+			n -= d
+			n //= 26
+		return res[::-1]
